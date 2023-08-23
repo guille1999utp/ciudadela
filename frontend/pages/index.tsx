b7ee0ca6dfd5eba67d50eb9293cc7e3bca1f9d31
guille1999utp <90706729+guille1999utp@users.noreply.github.com>
2023-08-22 23:06:48 -0500

    configuracion basica

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,14 @@
+import LayoutHomePage from '@/src/layouts/home';
+import Image from 'next/image';
+import { Box } from '@mui/material';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { AiOutlineSearch } from "react-icons/ai";
+import { MdLocationOn } from "react-icons/md";
+
+export default function Home() {
+  return (
+    <Box className="site-home">
+ dsf
+    </Box>
+  )
+}
