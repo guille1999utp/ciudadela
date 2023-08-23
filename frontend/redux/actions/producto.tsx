06dbc0a922a530c9537584b9f81fab6d036112cb
guille1999utp <90706729+guille1999utp@users.noreply.github.com>
2023-08-23 02:26:53 -0500

    eliminando redux

--- frontend/redux/actions/producto.tsx
+++
@@ -1,8 +0,0 @@
-import { TodoToggledPayload } from "@/interfaces/redux/search";
-import { PayloadAction } from "@reduxjs/toolkit";
-
-export const product = (state:any, action: PayloadAction<TodoToggledPayload>) => {
-
-
-}
-
--- frontend/redux/actions/search.tsx
+++
@@ -1,8 +0,0 @@
-import { TodoToggledPayload } from "@/interfaces/redux/search";
-import { PayloadAction } from "@reduxjs/toolkit";
-
-export const search = (state:any, action: PayloadAction<TodoToggledPayload>) => {
-
-
-}
-
